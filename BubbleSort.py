ada1d6f400bf4c9eeabb6192495dd0d54740c9fb
Brundabana Sahu <sbrundabana12@gmail.com>
2026-06-14 20:15:58 +0530

    Intial commit

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,9 +1,14 @@
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n-1):
+        didswap=0
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                didswap=1
+        if didswap==0:
+            break        
+
     return arr            
 
 
---
+++ Insertionsort.py
@@ -0,0 +1,29 @@
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        j=i
+        while j>0 and arr[j-1]>arr[j]:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+            j-=1
+            
+
+    return arr        
+
+
+
+
+
+
+
+
+
+n = int(input("Enter the size of the array:"))
+arr = list(map(int, input("Enter the elements:").split()))  
+
+
+
+insertion_sort(arr)
+
+print("Sorted array:")
+for i in arr:
+    print(i, end=" ") 
